cad2a9962b9cf60003a633005142c3036fc7e69b
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-06-01 10:15:44 +0500

    added login functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
-import router from './router.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./router.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-   
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,50 @@
-import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import { LoginPage } from "./pages/LoginPage";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import HomePage from "@/pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardLayout from "./layouts/DashboardLayout";
+import CreateBook from "./pages/CreateBook";
+import BooksPage from "./pages/BookPage";
+import AuthLayout from "./layouts/AuthLayut";
+import LoginPage from "./pages/LoginPage";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <Navigate to="/dashboard/home" />,
   },
   {
-    path: "/login",
-    element: <LoginPage/>,
-  }
+    path: "dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "books",
+        element: <BooksPage />,
+      },
+      {
+        path: "books/create",
+        element: <CreateBook />,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
 ]);
 
 export default router;
